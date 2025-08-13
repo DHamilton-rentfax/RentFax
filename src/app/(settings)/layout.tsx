ea4d81983e07d66715a0f01a848228f51383ed54
Dominique Hamilton <UrbnDesignz@gmail.com>
2025-08-13 16:05:22 +0000

    where is the header ?

--- src/app/(settings)/layout.tsx
+++ src/app/(settings)/layout.tsx
@@ -2,6 +2,7 @@ import Protected from '@/components/protected';
 import { Separator } from '@/components/ui/separator';
 import { SidebarNav } from '@/components/layout/sidebar-nav';
 import { Metadata } from 'next';
+import Header from '@/components/layout/header';
 
 export const metadata: Metadata = {
   title: 'Settings',
@@ -30,20 +31,25 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <Protected roles={['owner', 'manager']}>
-      <div className="container mx-auto p-4 md:p-10">
-        <div className="space-y-0.5">
-          <h1 className="text-2xl font-bold tracking-tight">Settings</h1>
-          <p className="text-muted-foreground">
-            Manage your company account settings and team members.
-          </p>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
-          <div className="flex-1 lg:max-w-4xl">{children}</div>
-        </div>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <Header />
+        <main className="flex-grow">
+          <div className="container mx-auto p-4 md:p-10">
+            <div className="space-y-0.5">
+              <h1 className="text-2xl font-bold tracking-tight">Settings</h1>
+              <p className="text-muted-foreground">
+                Manage your company account settings and team members.
+              </p>
+            </div>
+            <Separator className="my-6" />
+            <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+              <aside className="-mx-4 lg:w-1/5">
+                <SidebarNav items={sidebarNavItems} />
+              </aside>
+              <div className="flex-1 lg:max-w-4xl">{children}</div>
+            </div>
+          </div>
+        </main>
       </div>
     </Protected>
   );
